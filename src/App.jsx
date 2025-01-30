959374dd8cd5b2b94a6c2a4ea4eb4e0d43ccd97b
Javierguntara <gondeza2003@gmail.com>
2025-01-31 02:35:11 +0900

    perbaikan nama file-file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import styles from "./App.module.css";
-import { About } from "./components navbar/About/About";
-import { Contact } from "./components navbar/Contact/Contact";
-import { Experience } from "./components navbar/Experience/Experience";
-import { Hero } from "./components navbar/Hero/Hero";
-import { Navbar } from "./components navbar/Navbar/Navbar";
-import { Projects } from "./components navbar/Projects/Projects";
+import { About } from "./components-navbar/About/About";
+import { Contact } from "./components-navbar/Contact/Contact";
+import { Experience } from "./components-navbar/Experience/Experience";
+import { Hero } from "./components-navbar/Hero/Hero";
+import { Navbar } from "./components-navbar/Navbar/Navbar";
+import { Projects } from "./components-navbar/Projects/Projects";
 
 function App() {
   return (<div className={styles.App}>
